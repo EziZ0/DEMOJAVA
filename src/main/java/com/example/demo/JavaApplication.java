fc85a8a1a4f223b708416fb59f26eb599de7bbe8
ELZIN M <164911056+EziZ0@users.noreply.github.com>
2024-12-23 22:42:57 +0530

    Update JavaApplication.java

--- src/main/java/com/example/demo/JavaApplication.java
+++ src/main/java/com/example/demo/JavaApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//smg
 @SpringBootApplication
 public class JavaApplication {
 
